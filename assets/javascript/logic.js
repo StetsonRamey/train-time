0c97c59406bcb95fa24f022f388682cf0c337ce7
Stetson Ramey <stetson.ramey@gmail.com>
2018-03-01 17:37:38 -0600

    added all initial files, linked em all

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyB9jdGjUZRcU8yx7nRx4_2gJObUqHm0Tu0",
+  authDomain: "homework-7-train-time.firebaseapp.com",
+  databaseURL: "https://homework-7-train-time.firebaseio.com",
+  projectId: "homework-7-train-time",
+  storageBucket: "",
+  messagingSenderId: "57418653354"
+};
+firebase.initializeApp(config);
